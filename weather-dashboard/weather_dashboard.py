c52b419cb4c2cb8046d6668496fcbc2d9b1c41ef
fasrinrahman <mfasrinrahman@gmail.com>
2026-06-04 23:59:44 +0530

    feat(python): add weather dashboard with API integration

---
+++ weather-dashboard/weather_dashboard.py
@@ -0,0 +1,64 @@
+# =========================
+# MINI PROJECT 7
+# Weather Dashboard
+# =========================
+
+import requests
+
+
+def get_weather(city):
+
+    url = (
+        "https://wttr.in/"
+        + city
+        + "?format=j1"
+    )
+
+    try:
+
+        response = requests.get(url)
+
+        data = response.json()
+
+        current = data["current_condition"][0]
+
+        print("\n===== WEATHER REPORT =====")
+
+        print("City:", city)
+
+        print(
+            "Temperature:",
+            current["temp_C"],
+            "°C"
+        )
+
+        print(
+            "Humidity:",
+            current["humidity"],
+            "%"
+        )
+
+        print(
+            "Weather:",
+            current["weatherDesc"][0]["value"]
+        )
+
+    except Exception as error:
+
+        print(
+            "Error occurred:",
+            error
+        )
+
+
+while True:
+
+    city = input(
+        "\nEnter city name (or exit): "
+    )
+
+    if city.lower() == "exit":
+
+        break
+
+    get_weather(city)
